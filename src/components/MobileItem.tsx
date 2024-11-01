172c1c4152fc81ef8b65404848204c04090d4d91
Raheem Orekoya <tundeorekoya1@gmail.com>
2024-11-01 02:42:54 +0100

    some changes

--- src/components/MobileItem.tsx
+++ src/components/MobileItem.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { LucideProps } from "lucide-react"
 import { ForwardRefExoticComponent, RefAttributes } from "react"
 import Link from "next/link"
@@ -15,20 +17,18 @@ interface MobileItemProps {
 const MobileItem = ({ label, icon: Icon, href, active }: MobileItemProps) => {
    const router = useRouter()
    
+   const handleClick = async () => {
+      if(label === "Logout") {
+         await logout()
+         return
+      }
+   }
+   
    return (
-      <div onClick={async () => {
-         if(label === "Logout") {
-            await logout()
-            return
-         }
-         router.push(`${href}`)
-      }} className="flex-1 h-full border-t"
-      >
-         <Link href={href} className={cn("w-full h-full flex flex-col items-center justify-center gap-1 text-muted-foreground/50 hover:text-primary", { "text-primary font-medium": active })}>
-            <Icon className="flex-shrink-0 w-5 h-5" strokeWidth={1.5} />
-            <span className={cn("text-xs text-center")}>{label}</span>
-         </Link>
-      </div>
+      <Link href={href} onClick={handleClick} className={cn("w-full h-full bg-blue-300 flex flex-col items-center justify-center gap-1 text-muted-foreground/50 hover:text-primary", { "text-primary font-medium": active })}>
+         <Icon className="flex-shrink-0 w-5 h-5" strokeWidth={1.5} />
+         <span className={cn("text-xs text-center")}>{label}</span>
+      </Link>
    )
 }
 
